ef678c69c05876e49427df25f9f0a6a5bfd9c6cc
Killian Gemoets <kilgem@live.fr>
2022-04-14 12:01:48 +0200

    index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,7 @@
 var express = require("express");
 var router = express.Router();
-var journeyModel = require('../models/journey');
-const mongoose = require('mongoose');
-
+var journeyModel = require("../models/journey");
+const mongoose = require("mongoose");
 
 // useNewUrlParser ;)
 var options = {
@@ -14,10 +13,10 @@ var options = {
 // --------------------- BDD -----------------------------------------------------
 
 const password = "El7mQWrLv3gIiiws";
-mongoose.connect(`mongodb+srv://admin:${password}@cluster0.fvcy8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-   options,
-   function(err) {
-
+mongoose.connect(
+  `mongodb+srv://admin:${password}@cluster0.fvcy8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  options,
+  function (err) {
     if (err) {
       console.log(
         `error, failed to connect to the database because --> ${err}`
@@ -30,7 +29,6 @@ mongoose.connect(`mongodb+srv://admin:${password}@cluster0.fvcy8.mongodb.net/myF
 
 // BELLOW comment content TO DELETE ! CONTENT ADDED IN models/journey.JS
 
-
 // var journeySchema = mongoose.Schema({
 //   departure: String,
 //   arrival: String,
@@ -40,15 +38,38 @@ mongoose.connect(`mongodb+srv://admin:${password}@cluster0.fvcy8.mongodb.net/myF
 // });
 // var journeyModel = mongoose.model('journey', journeySchema);
 
-var city = ["Paris","Marseille","Nantes","Lyon","Rennes","Melun","Bordeaux","Lille"]
-var date = ["2018-11-20","2018-11-21","2018-11-22","2018-11-23","2018-11-24"]
-
-
+var city = [
+  "Paris",
+  "Marseille",
+  "Nantes",
+  "Lyon",
+  "Rennes",
+  "Melun",
+  "Bordeaux",
+  "Lille",
+];
+var date = [
+  "2018-11-20",
+  "2018-11-21",
+  "2018-11-22",
+  "2018-11-23",
+  "2018-11-24",
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('models', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index");
+});
 
+router.get("/home", function (req, res, next) {
+  res.render("home");
+});
+
+router.get("/results", function (req, res, next) {
+  res.render("results");
+});
+router.get("/tickets", function (req, res, next) {
+  res.render("tickets");
 });
 
 // Remplissage de la base de donnée, une fois suffit
@@ -92,8 +113,7 @@ router.get("/result", function (req, res, next) {
       }
     );
   }
-  res.render('index', { title: 'Express' });
-
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
