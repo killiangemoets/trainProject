5db3589ce359a0e36b2cfc983bcad730c2e44d88
Killian Gemoets <kilgem@live.fr>
2022-04-15 14:05:00 +0200

    fridayAfternoon

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,16 @@ var journeyModel = require("../models/journey");
 var userModel = require("../models/user");
 const mongoose = require("mongoose");
 
+// Function to check if an object is empty (come from the internet, not mine) -> Return true if empty, false if not
+function isEmpty(obj) {
+  for (var prop in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+      return false;
+    }
+  }
+  return JSON.stringify(obj) === JSON.stringify({});
+}
+
 // useNewUrlParser ;)
 
 var city = [
@@ -94,11 +104,30 @@ router.get("/home", function (req, res, next) {
 });
 
 router.get("/tickets", async function (req, res, next) {
-  if (req.session.user == null) return res.redirect("/"); 
-    if (!req.session?.tickets) req.session.tickets = [];
-    req.session.tickets.push(await journeyModel.findById(req.query.id));
+  if (req.session.user == null) return res.redirect("/");
+  if (!req.session?.tickets) req.session.tickets = [];
+  req.session.tickets.push(await journeyModel.findById(req.query.id));
 
-    res.render("tickets", { tickets: req.session.tickets })
+  res.render("tickets", { tickets: req.session.tickets });
+});
+
+router.get("/delete", async function (req, res, next) {
+  if (req.session.user == null) return res.redirect("/");
+
+  console.log(req.query.id);
+  if (!isEmpty(req.query)) {
+    console.log("here");
+    console.log(req.session.tickets);
+
+    // If we have 2 times the same ticket, we only want to delete one.
+    const firstPosition = req.session.tickets.findIndex(
+      (ticket) => ticket._id == req.query.id
+    );
+    req.session.tickets.splice(firstPosition, 1);
+  }
+
+  res.render("tickets", { tickets: req.session.tickets });
+  // res.redirect("/tickets");
 });
 
 router.get("/lasttrips", function (req, res, next) {
@@ -113,7 +142,6 @@ router.get("/confirmation", function (req, res, next) {
   if (req.session.user == null) {
     res.redirect("/");
   } else {
-    
     res.render("confirmation");
   }
 });
@@ -127,7 +155,6 @@ router.get("/notrain", function (req, res, next) {
 });
 
 router.post("/results", async function (req, res, next) {
-
   // On fait ca pour la convertir en date car de base c'est un string
   const date = new Date(req.body.date);
 
